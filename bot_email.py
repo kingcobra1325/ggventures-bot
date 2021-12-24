56cbd0e1a572940a9033610683cb99ed2be67df3
kingcobra2alt <kingcobra1325@gmail.com>
2021-12-24 08:59:33 +0800

    Updated bot_email

--- bot_email.py
+++ bot_email.py
@@ -14,47 +14,44 @@ dev_recipients = ['goldengooseventures.developer@gmail.com', 'kingcobra1325@gmai
 
 def missing_info_email(spider="Default Spider", university_name="Default University", missing_info=['missing','info'], web_link="www.google.com"):
     try:
-        unpack = ''
-        for x in missing_info:
-            unpack = f"{unpack}<p>{x}</p>"
-
         # EMAIL INIT
-        with smtplib.SMTP(SMTP_SERVER, int(SMTP_PORT)) as mail:
-            mail.ehlo()
-            mail.starttls()
-            mail.login(SMTP_EMAIL,SMTP_KEY)
+        for recipient in dev_recipients:
+            with smtplib.SMTP(SMTP_SERVER, int(SMTP_PORT)) as mail:
+                mail.ehlo()
+                mail.starttls()
+                mail.login(SMTP_EMAIL,SMTP_KEY)
 
-            msg = MIMEMultipart('alternative')
-            msg['Subject'] = f'GGV BOT Missing Information - {spider}'
-            msg['To'] = ", ".join(dev_recipients)
-            msg['From'] = 'goldengooseventures.developer@gmail.com'
+                msg = MIMEMultipart('alternative')
+                msg['Subject'] = f'GGV BOT Missing Information - {spider}'
+                msg['To'] = recipient
+                msg['From'] = 'goldengooseventures.developer@gmail.com'
 
-            # EMAIL CONTENT
-            html = f"""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-            <html xmlns="http://www.w3.org/1999/xhtml">
-             <head>
-              <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-              <title>Missing Information from {spider} - {university_name}</title>
-              <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            </head>
-            <body style="background-color:black;color:white">
-            <h1 style="text-align:center">Missing Information - {university_name}</h1>
-            <h4 style="color:white">The Spider: {spider} failed to scrape multiple information from {university_name}
-            <p>Missing Information: </p>
-            {unpack}
-            <br>
-            <p>Please check if all the information scraped are correct</p>
-            <p>Link: {web_link}</p>
-            <br>
-            </h4>
-            <h5 style="text-align:center">Email Timestamp: {datetime.utcnow().strftime('%m-%d-%Y %I:%M:%S %p')}</h5>
-            </body>
-            </html>"""
+                # EMAIL CONTENT
+                html = f"""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+                <html xmlns="http://www.w3.org/1999/xhtml">
+                 <head>
+                  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                  <title>Missing Information from {spider} - {university_name}</title>
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                </head>
+                <body style="background-color:black;color:white">
+                <h1 style="text-align:center">Missing Information - {university_name}</h1>
+                <h4 style="color:white">The Spider: {spider} failed to scrape multiple information from {university_name}
+                <p>Missing Information: </p>
+                {unpack}
+                <br>
+                <p>Please check if all the information scraped are correct</p>
+                <p>Link: {web_link}</p>
+                <br>
+                </h4>
+                <h5 style="text-align:center">Email Timestamp: {datetime.utcnow().strftime('%m-%d-%Y %I:%M:%S %p')}</h5>
+                </body>
+                </html>"""
 
-            msg.attach(MIMEText(html,'html'))
+                msg.attach(MIMEText(html,'html'))
 
-            mail.sendmail(msg['From'], msg['To'], msg.as_string())
-            logger.error(f'Missing Information Email from {spider} successfully sent to {msg["To"]}')
+                mail.sendmail(msg['From'], msg['To'], msg.as_string())
+                logger.error(f'Missing Information Email from {spider} successfully sent to {msg["To"]}')
 
     except Exception as e:
         logger.error("Exception when calling Email Bot->: %s\n" % e)
@@ -62,41 +59,42 @@ def missing_info_email(spider="Default Spider", university_name="Default Univers
 def error_email(spider="Default Spider",error="Default Error"):
     try:
         # EMAIL INIT
-        with smtplib.SMTP(SMTP_SERVER, int(SMTP_PORT)) as mail:
-            mail.ehlo()
-            mail.starttls()
-            mail.login(SMTP_EMAIL,SMTP_KEY)
+        for recipient in dev_recipients:
+            with smtplib.SMTP(SMTP_SERVER, int(SMTP_PORT)) as mail:
+                mail.ehlo()
+                mail.starttls()
+                mail.login(SMTP_EMAIL,SMTP_KEY)
 
-            msg = MIMEMultipart('alternative')
-            msg['Subject'] = f'GGV BOT ERROR - {spider}'
-            msg['To'] = ", ".join(dev_recipients)
-            msg['From'] = 'goldengooseventures.developer@gmail.com'
+                msg = MIMEMultipart('alternative')
+                msg['Subject'] = f'GGV BOT ERROR - {spider}'
+                msg['To'] = recipient
+                msg['From'] = 'goldengooseventures.developer@gmail.com'
 
-            # EMAIL CONTENT
-            html = f"""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-            <html xmlns="http://www.w3.org/1999/xhtml">
-             <head>
-              <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-              <title>GGV BOT ERROR - {spider}</title>
-              <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            </head>
-            <body style="background-color:black;color:white">
-            <h1 style="text-align:center">Spider experienced an Error!</h1>
-            <h4 style="color:white">The Spider: {spider} experienced an error while running
-            <p>Error Details: </p>
-            {error}
-            <br>
-            <p>Please check the code as soon as possible to address the issue as soon as possible!</p>
-            <br>
-            </h4>
-            <h5 style="text-align:center">Error Timestamp: {datetime.utcnow().strftime('%m-%d-%Y %I:%M:%S %p')}</h5>
-            </body>
-            </html>"""
+                # EMAIL CONTENT
+                html = f"""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+                <html xmlns="http://www.w3.org/1999/xhtml">
+                 <head>
+                  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                  <title>GGV BOT ERROR - {spider}</title>
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                </head>
+                <body style="background-color:black;color:white">
+                <h1 style="text-align:center">Spider experienced an Error!</h1>
+                <h4 style="color:white">The Spider: {spider} experienced an error while running
+                <p>Error Details: </p>
+                {error}
+                <br>
+                <p>Please check the code as soon as possible to address the issue as soon as possible!</p>
+                <br>
+                </h4>
+                <h5 style="text-align:center">Error Timestamp: {datetime.utcnow().strftime('%m-%d-%Y %I:%M:%S %p')}</h5>
+                </body>
+                </html>"""
 
-            msg.attach(MIMEText(html,'html'))
+                msg.attach(MIMEText(html,'html'))
 
-            mail.sendmail(msg['From'], msg['To'], msg.as_string())
-            logger.error(f'Error Email from {spider} successfully sent to {msg["To"]}')
+                mail.sendmail(msg['From'], msg['To'], msg.as_string())
+                logger.error(f'Error Email from {spider} successfully sent to {msg["To"]}')
 
     except Exception as e:
         logger.error("Exception when calling Email Bot->: %s\n" % e)
