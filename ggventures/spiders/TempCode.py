186277f3604b74926beaf4ec78160233b303fda7
Namaedaa <joachim.cobar@gmail.com>
2022-01-09 01:28:07 +0800

    Updated Spiders

--- ggventures/spiders/TempCode.py
+++ ggventures/spiders/TempCode.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
     from selenium.webdriver.support import expected_conditions as EC
     from selenium.common.exceptions import NoSuchElementException
     from selenium.webdriver.common.action_chains import ActionChains
+    
+    import re
         
-    link = "http://specialevents.csulb.edu/mastercalendar/MasterCalendar.aspx"
-
     event_name = list()
     event_date = list()
     event_time = list()
@@ -18,48 +18,94 @@ if __name__ == '__main__':
     options = webdriver.ChromeOptions()
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
     options.add_argument(f'user-agent={user_agent}')
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     options.add_argument('--log-level 3') 
-    driver = webdriver.Chrome(executable_path='C:\Chromium\chromedriver',options=options)
+    driver = webdriver.Chrome(executable_path='C:\Chromium97\chromedriver',options=options)
     # getter = webdriver.Chrome(executable_path='C:\Chromium\chromedriver',options=options)
+    # getter2 = webdriver.Chrome(executable_path='C:\Chromium\chromedriver',options=options)
+    
+    link = "https://www.business.msstate.edu/events/2021/12/martin-luther-king-jr-holiday-mississippi-state-university-closed"
+        
     driver.get(link)
-    height = driver.execute_script("return document.body.scrollHeight")
     
-    EventLinks = WebDriverWait(driver,60).until(EC.presence_of_all_elements_located((By.XPATH,"//div[contains(@class,'items-scroller')]/div")))
-
-    Stop = driver.find_element(By.XPATH, "//div[contains(@title,'Pause')]")
-    Stop.click()
+    RawEventDesc = (WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH,"//div[contains(@class,'summary')]")))).text
     
-    for i in EventLinks:
-        i.click()
-               
-        EventPopUp = WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH,"//div[contains(@class,'six columns scroller-text')]/h1")))
-        
-        EventPopUp.click()
-        
-        RawEventName = (WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH,"//div[contains(@class,'row pl10 pr20')]/h2")))).text
-               
-        RawEventDesc =  (WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH,"//div[starts-with(@id, 'eventDescriptionEventDetails')]")))).text
-        
-        RawEventDate =  (WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH,"//div[contains(@class,'row pl10 pr20')]//div[contains(@class,'info-date')]")))).text
-        
-        RawEventTime =  (WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH,"//div[contains(@class,'row pl10 pr20')]//div[contains(@class,'info-time fl')]")))).text
-        
-        RawEventLink =  (WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH, "(//div[contains(@class,'row pl10 pr20')]//div[contains(@class,'font-weight-normal')])[3]")))).text
+    print(RawEventDesc.text)
 
-        PopClose = driver.find_element(By.XPATH, "//a[contains(@title,'Close')]").click()
+
+
+    # Logo = (WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH,"//h2/a")))).value_of_css_property("background")
+    # Logo = re.findall(r'''\"(\S+)\"''',Logo)[0]
+    
+    # print(Logo)
+
         
-        try:
-            driver.find_element(By.XPATH, "//div[contains(@class,'prev-next pointer next-item')]").click()
-        except:
-            break
+        # for i in EventLinks:
+        #     getter.get(i.get_attribute('href'))
+                
+        #     RawEventName = (WebDriverWait(getter,60).until(EC.presence_of_element_located((By.XPATH,"//span[contains(@class,'heading__light')]")))).text
+            
+        #     RawEventDesc = getter.find_element(By.XPATH,"//div[contains(@class,'wysiwyg-container')]").text
+
+        #     RawEventDate = getter.find_element(By.XPATH,"//div[contains(@class,'event-card__date--inner')]").text
+            
+        #     try:
+        #         RawEventTime = getter.find_element(By.XPATH,"//div[contains(@class,'event__label--wrapper')]/span[2]").text
+        #     except:
+        #         RawEventTime = None
+        #     print(RawEventName)
         
         
-        print(RawEventName)
-        print(RawEventDesc)
-        print(RawEventDate)
-        print(RawEventTime)
-        print(RawEventLink)
+    # for o in range(3):
+    #     Element = (WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH,"//li[contains(@class,'school-drucker-school-of-management')]/input"))))
+    #     driver.execute_script("arguments[0].scrollIntoView();", Element)
+    #     Element.click()
+    #     sleep(3)
+    #     TestLinks = driver.find_elements(By.XPATH,"//a[contains(@class,'event__url')]")
+    #     print(len(TestLinks))
+    #     Links = driver.find_elements(By.XPATH,"//a[contains(@class,'event__url')]")
+    #     for i in Links:
+    #         print(i.text) 
+    #     driver.get(driver.find_element(By.XPATH,"//a[contains(@rel,'next')]").get_attribute('href'))
+    # sleep(100)
+    # for i in EventLinks:
+    #     print(i.get_attribute('href'))
+    
+    # EventLinks = WebDriverWait(driver,60).until(EC.presence_of_all_elements_located((By.XPATH,"//div[contains(@class,'items-scroller')]/div")))
+
+    # Stop = driver.find_element(By.XPATH, "//div[contains(@title,'Pause')]")
+    # Stop.click()
+    
+    # for i in EventLinks:
+    #     i.click()
+               
+    #     EventPopUp = WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH,"//div[contains(@class,'six columns scroller-text')]/h1")))
+        
+    #     EventPopUp.click()
+        
+    #     RawEventName = (WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH,"//div[contains(@class,'row pl10 pr20')]/h2")))).text
+               
+    #     RawEventDesc =  (WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH,"//div[starts-with(@id, 'eventDescriptionEventDetails')]")))).text
+        
+    #     RawEventDate =  (WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH,"//div[contains(@class,'row pl10 pr20')]//div[contains(@class,'info-date')]")))).text
+        
+    #     RawEventTime =  (WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH,"//div[contains(@class,'row pl10 pr20')]//div[contains(@class,'info-time fl')]")))).text
+        
+    #     RawEventLink =  (WebDriverWait(driver,60).until(EC.presence_of_element_located((By.XPATH, "(//div[contains(@class,'row pl10 pr20')]//div[contains(@class,'font-weight-normal')])[3]")))).text
+
+    #     PopClose = driver.find_element(By.XPATH, "//a[contains(@title,'Close')]").click()
+        
+    #     try:
+    #         driver.find_element(By.XPATH, "//div[contains(@class,'prev-next pointer next-item')]").click()
+    #     except:
+    #         break
+        
+        
+    #     print(RawEventName)
+    #     print(RawEventDesc)
+    #     print(RawEventDate)
+    #     print(RawEventTime)
+    #     print(RawEventLink)
         
         
         
--- ggventures/spiders/usa_0043.py
+++ ggventures/spiders/usa_0043.py
@@ -1,10 +1,99 @@
-import scrapy
+import scrapy, time
+# from scrapy import Selector
 
+from bot_email import missing_info_email, error_email
+
+from binaries import Load_Driver, logger, WebScroller
+
+from scrapy.loader import ItemLoader
+
+from ggventures.items import GgventuresItem
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class Usa0043Spider(scrapy.Spider):
     name = 'usa_0043'
-    allowed_domains = ['https://www.lmu.edu/']
-    start_urls = ['http://https://www.lmu.edu//']
+    country = 'US'
+    # start_urls = ["https://cba.k-state.edu/about/events/"]
+    start_urls = ["https://cal.lmu.edu/calendar"]
+
+    def __init__(self):
+        self.driver = Load_Driver()
+        self.getter = Load_Driver()
+        self.start_time = round(time.time())
+        self.scrape_time = None
 
     def parse(self, response):
-        pass
+        try:
+            event_name = list()
+            event_date = list()
+            event_time = list()
+            event_desc = list()
+            event_link = list()
+
+            self.driver.get("https://www.lmu.edu/")
+
+            logo = 'https://brand.lmu.edu/media/wnmd/brand/templatesanddownloads/logodownloads/Fullname-LeftAligned_RGB-CrimsonBlue.jpg'
+            # logo = re.findall(r'''\"(\S+)\"''',logo)[0]
+
+            university_name = "Loyola Marymount University"
+
+            university_contact_info = (WebDriverWait(self.driver,60).until(EC.presence_of_element_located((By.XPATH,"//div[contains(@class,'columns__item')]")))).text
+
+            self.driver.get(response.url)
+
+            EventLinks = WebDriverWait(self.driver,60).until(EC.presence_of_all_elements_located((By.XPATH,"//h3[@class='em-card_title']/a")))
+            for i in EventLinks:
+                self.getter.get(i.get_attribute('href'))
+
+                RawEventName = (WebDriverWait(self.getter,60).until(EC.presence_of_element_located((By.XPATH,"//h1[@class='em-header-card_title']")))).text
+
+                try:
+                    RawEventDesc = self.getter.find_element(By.XPATH,"//div[@class='em-about_description']").text
+                except:
+                    RawEventDesc = None
+
+                RawEventDate = self.getter.find_element(By.XPATH,"//div[@class='em-list_dates__container']").text
+
+                try:
+                    RawEventTime = self.getter.find_element(By.XPATH,"//div[@class='em-list_dates__container']").text
+                except:
+                    RawEventTime = None
+
+
+                event_name.append(RawEventName)
+                event_desc.append(RawEventDesc)
+                event_date.append(RawEventDate)
+                event_time.append(RawEventTime)
+                event_link.append(i.get_attribute('href'))
+
+
+
+            for i in range(len(event_name)):
+                data = ItemLoader(item = GgventuresItem(), selector = i)
+                data.add_value('university_name',university_name)
+                data.add_value('university_contact_info',university_contact_info)
+                data.add_value('logo',logo)
+                data.add_value('event_name', event_name[i])
+                data.add_value('event_desc', event_desc[i])
+                data.add_value('event_date', event_date[i])
+                data.add_value('event_time', event_time[i])
+                data.add_value('event_link', event_link[i])
+
+                yield data.load_item()
+
+        except Exception as e:
+            logger.error(f"Experienced error on Spider: {self.name} --> {e}. Sending Error Email Notification")
+            error_email(self.name,e)
+    def closed(self, reason):
+        try:
+            self.driver.quit()
+            self.getter.quit()
+            self.scrape_time = str(round(((time.time() - self.start_time) / float(60)), 2)) + ' minutes' if (time.time() - self.start_time > 60.0) else str(round(time.time() - self.start_time)) + ' seconds'
+            logger.debug(f"Spider: {self.name} scraping finished due to --> {reason}")
+            logger.debug(f"Elapsed Scraping Time: {self.scrape_time}")
+        except Exception as e:
+            logger.error(f"Experienced error while closing Spider: {self.name} with reason: {reason} --> {e}. Sending Error Email Notification")
+            error_email(self.name,e)
