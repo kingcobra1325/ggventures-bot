ea73bf6a153070143c9d5e10e79dac82ae0eb2da
Namaedaa <joachim.cobar@gmail.com>
2022-02-09 23:48:23 +0800

    Updated Spiders

--- ggventures/spiders/gbr_0038.py
+++ ggventures/spiders/gbr_0038.py
@@ -1,10 +1,101 @@
 import scrapy
+import scrapy, time
+# from scrapy import Selector
 
+from bot_email import missing_info_email, error_email
+
+from binaries import Load_Driver, logger, WebScroller
+
+from scrapy.loader import ItemLoader
+
+from ggventures.items import GgventuresItem
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class Gbr0038Spider(scrapy.Spider):
     name = 'gbr_0038'
-    allowed_domains = ['https://business.leeds.ac.uk/']
-    start_urls = ['http://https://business.leeds.ac.uk//']
+    country = 'United Kingdom'
+    # start_urls = ["https://cba.k-state.edu/about/events/"]
+    start_urls = ["https://business.leeds.ac.uk/events?categoryID=1"]
+
+    def __init__(self):
+        self.driver = Load_Driver()
+        self.getter = Load_Driver()
+        self.start_time = round(time.time())
+        self.scrape_time = None
 
     def parse(self, response):
-        pass
+        try:
+            self.driver.get("https://business.leeds.ac.uk/")
+
+            logo = "https://www.efmdglobal.org/wp-content/uploads/LUBS-logo-high-res-1024x374N.png"
+            # logo = re.findall(r'''\"(\S+)\"''',logo)[0]
+
+            university_name = "University of Leeds,Leeds University Business School"
+            
+            self.driver.get("http://business.leeds.ac.uk/faculty/doc/contact-us")
+            
+            # self.driver.find_element(By.XPATH,"//*[contains(text(),'General contacts')]").click()
+            
+            university_contact_info = (WebDriverWait(self.driver,60).until(EC.presence_of_element_located((By.XPATH,"//div[@class='cms']"))))
+
+            self.driver.get(response.url)     
+        
+            counter = 0
+            EventLinks = WebDriverWait(self.driver,60).until(EC.presence_of_all_elements_located((By.XPATH,"//h4[@class='heading-link']/a")))
+            for i in EventLinks:
+                self.getter.get(i.get_attribute('href'))
+
+                RawEventName = (WebDriverWait(self.getter,60).until(EC.presence_of_element_located((By.XPATH,"//h1[@class='heading-underline']")))).text
+
+                try:
+                    RawEventDesc = self.getter.find_element(By.XPATH,"//div[@class='cms']").text
+                except:
+                    RawEventDesc = None
+
+                try:
+                    RawEventDate = self.getter.find_element(By.XPATH,"//strong[text()='Date']/..").text
+                except:
+                    RawEventDate = None
+                    
+                try:
+                    # RawEventTime = None                    
+                    # RawEventTime = self.getter.find_element(By.XPATH,"//p[@Class='event__time']").text 
+                    RawEventTime = RawEventDate
+                except:
+                    RawEventTime = None
+                    
+                # try:
+                #     RawStartContactInfo = self.getter.find_element(By.XPATH,"//dt[text()='Contact']/..").text
+                # except:
+                #     RawStartContactInfo = None
+
+                data = ItemLoader(item = GgventuresItem(), selector = counter)
+                data.add_value('university_name',university_name)
+                data.add_value('university_contact_info',university_contact_info)
+                data.add_value('logo',logo)
+                data.add_value('event_name', RawEventName)
+                data.add_value('event_desc', RawEventDesc)
+                data.add_value('event_date', RawEventDate)
+                data.add_value('event_time', RawEventTime)
+                data.add_value('event_link', i.get_attribute('href'))
+                # data.add_value('startups_contact_info', RawStartContactInfo)
+                counter+=1
+
+                yield data.load_item()
+
+        except Exception as e:
+            logger.error(f"Experienced error on Spider: {self.name} --> {e}. Sending Error Email Notification")
+            error_email(self.name,e)
+    def closed(self, reason):
+        try:
+            self.driver.quit()
+            self.getter.quit()
+            self.scrape_time = str(round(((time.time() - self.start_time) / float(60)), 2)) + ' minutes' if (time.time() - self.start_time > 60.0) else str(round(time.time() - self.start_time)) + ' seconds'
+            logger.debug(f"Spider: {self.name} scraping finished due to --> {reason}")
+            logger.debug(f"Elapsed Scraping Time: {self.scrape_time}")
+        except Exception as e:
+            logger.error(f"Experienced error while closing Spider: {self.name} with reason: {reason} --> {e}. Sending Error Email Notification")
+            error_email(self.name,e)
