8e61d2b0c1279036fc28af8af5fb25f3261ed4a2
Namaedaa <joachim.cobar@gmail.com>
2022-02-10 12:14:12 +0800

    Updated pattern

--- patterns.py
+++ patterns.py
@@ -8,10 +8,12 @@ STARTUP_LINK_PATTERNS =             [
 
 DATE_PATTERNS_RE =                  [
                                         # r"(January|February|March|April|May|June|July|August|September|October|November|December)/s/d/d[,]/d/d/d/d",
+    
                                         r"(?:January|February|March|April|May|June|July|August|September|October|November|December)[ ][0-9][0-9][,][ ][0-9][0-9][0-9][0-9]",
                                         r"(?:JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER)[ ][0-9][0-9][,][ ][0-9][0-9][0-9][0-9]",
                                         r"(?:January|February|March|April|May|June|July|August|September|October|November|December)[ ][0-9][,][ ][0-9][0-9][0-9][0-9]",
                                         r"(?:JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER)[ ][0-9][,][ ][0-9][0-9][0-9][0-9]",
+                                        r"[0-9][0-9][ ](?:January|February|March|April|May|June|July|August|September|October|November|December)[ ][0-9][0-9][0-9][0-9]",
                                         r"(?:JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER)[ ][0-9][0-9]",
                                         r"(?:January|February|March|April|May|June|July|August|September|October|November|December)[ ][0-9][0-9]",
                                         r"(?:January|February|March|April|May|June|July|August|September|October|November|December)[ ][0-9]",
@@ -25,18 +27,20 @@ DATE_PATTERNS_RE =                  [
                                         r"(?:JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEPT|OCT|NOV|DEC|JAN.|FEB.|MAR.|APR.|MAY.|JUN.|JUL.|AUG.|SEPT.|OCT.|NOV.|DEC.|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sept|Oct|Nov|Dec|Jan.|Feb.|Mar.|Apr.|May.|Jun.|Jul.|Aug.|Sept.|Oct.|Nov.|Dec.|jan.|feb.|mar.|apr.|may.|jun.|jul.|aug.|sept.|oct.|nov.|dec.|jan|feb|mar|apr|may|jun|jul|aug|sept|oct|nov|dec)[ ][0-9]",
                                         r"[0-9]/[0-9]/[0-9][0-9][0-9][0-9]",
                                         r"[0-9][0-9]\s(?:JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER|January|February|March|April|May|June|July|August|September|October|November|December),\s[0-9][0-9][0-9][0-9]",
+                                        r"[0-9][0-9][ ](?:January|February|March|April|May|June|July|August|September|October|November|December)",
                                     ]
 
 date_strf_pattern = '%m/%d/%Y'
 
 DATE_PATTERNS =                     [
-                                        ["%B %d, %Y",date_strf_pattern],
-                                        ["%m/%d/%Y",date_strf_pattern],
-                                        ["%B %d",'%m/%d'],
-                                        ["%Y-%m-%d",date_strf_pattern],
-                                        ["%b %d, %Y",date_strf_pattern],
-                                        ["%b %d",'%m/%d'],
-                                        ["%d %B, %Y",date_strf_pattern],
+                                        ["%B %d, %Y"        ,date_strf_pattern],
+                                        ["%m/%d/%Y"         ,date_strf_pattern],
+                                        ["%B %d"            ,'%m/%d'],
+                                        ["%Y-%m-%d"         ,date_strf_pattern],
+                                        ["%b %d, %Y"        ,date_strf_pattern],
+                                        ["%b %d"            ,'%m/%d'],
+                                        ["%d %B, %Y"        ,date_strf_pattern],
+                                        ["%d %B %Y"         ,date_strf_pattern],
                                     ]
 
 TIME_PATTERNS_RE =                  [
@@ -71,12 +75,12 @@ TIME_PATTERNS_RE =                  [
 time_strf_pattern = '%I:%M:%S %p'
 
 TIME_PATTERNS =                     [
-                                        ["%I%p",time_strf_pattern],
-                                        ["%I %p",time_strf_pattern],
-                                        ["%I:%M %p",time_strf_pattern],
-                                        ["%I:%M", "'%I:%M:%S"],
-                                        ["%I:%M%p",time_strf_pattern],
-                                        ["T%H:%M:%SZ",time_strf_pattern],
+                                        ["%I%p"         ,time_strf_pattern],
+                                        ["%I %p"        ,time_strf_pattern],
+                                        ["%I:%M %p"     ,time_strf_pattern],
+                                        ["%I:%M"        , "'%I:%M:%S"],
+                                        ["%I:%M%p"      ,time_strf_pattern],
+                                        ["T%H:%M:%SZ"   ,time_strf_pattern],
                                     ]
 
 TZ_PATTERNS =                       [
