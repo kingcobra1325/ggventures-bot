05981c84e414790dc0ee3efdc361f7c5373451f0
kingcobra2alt <kingcobra1325@gmail.com>
2022-02-25 06:36:19 +0800

    updated spider_template aus_0019

--- ggventures/spiders/aus_0019.py
+++ ggventures/spiders/aus_0019.py
@@ -17,18 +17,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
-# def counter(function):
-#     """
-#     Wraps function with a Try-Except Catch
-#     """
-#     def wrapper(*args,**kwargs):
-#         try:
-#             function(*args,**kwargs)
-#         except Exception as e:
-#             logger.error(f"Experienced error on Spider: {self.name} --> {e}. Sending Error Email Notification")
-#             error_email(self.name,e)
-#     return wrapper
-
 
 class Aus0019Spider(GGVenturesSpider):
     name = 'aus_0019'
@@ -43,48 +31,46 @@ class Aus0019Spider(GGVenturesSpider):
 
     parse_code_link = 'https://cbe.anu.edu.au/events'
 
-    def get_university_contact_info(self,response):
-        self.driver.get(response.url)
-        self.university_contact_info = (WebDriverWait(self.driver,60).until(EC.presence_of_element_located((By.XPATH, "//section[contains(@id,'contactdetails')]")))).text
+    university_contact_info_xpath = "//section[contains(@id,'contactdetails')]"
+    contact_info_text = True
+    # contact_info_textContent = False
 
     def parse_code(self,response):
-        self.driver.get(response.url)
-        EventLinks = WebDriverWait(self.driver,20).until(EC.presence_of_all_elements_located((By.XPATH,"//div[contains(@class,'news-right')]/h3/a")))
+        try:
+        ####################
+            self.driver.get(response.url)
 
-        for i in EventLinks:
+            for link in self.events_list(event_links_xpath="//div[contains(@class,'news-right')]/h3/a"):
 
-            data = ItemLoader(item = GgventuresItem(), selector = i)
+                self.getter.get(link)
 
-            link = i.get_attribute('href')
-            self.getter.get(link)
+                if self.unique_event_checker(url_substring='anu.edu.au/events'):
 
-            if self.unique_event_checker(url_substring='anu.edu.au/events'):
+                    logger.info(f"Currently scraping --> {self.getter.current_url}")
 
-                logger.info(f"Currently scraping --> {self.getter.current_url}")
+                    item_data = self.item_data_empty.copy()
 
-                item_data = self.item_data_empty.copy()
-
-                item_data['event_name'] = WebDriverWait(self.getter,20).until(EC.presence_of_element_located((By.XPATH,"//h1"))).get_attribute('textContent')
-                try:
-                    item_data['event_desc'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'event-left-col')]").text
-                except NoSuchElementException as e:
-                    logger.debug(f"Error: {e}. Using an Alternate Scraping XPATH....")
-                    item_data['event_desc'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'structured-content-rich-text')]").text
+                    item_data['event_name'] = WebDriverWait(self.getter,20).until(EC.presence_of_element_located((By.XPATH,"//h1"))).get_attribute('textContent')
+                    try:
+                        item_data['event_desc'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'event-left-col')]").text
+                    except NoSuchElementException as e:
+                        logger.debug(f"Error: {e}. Using an Alternate Scraping XPATH....")
+                        item_data['event_desc'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'structured-content-rich-text')]").text
 
 
-                try:
-                    item_data['event_date'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'event-right-col')]").text
-                    item_data['event_time'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'event-right-col')]").text
-                except NoSuchElementException as e:
-                    logger.debug(f"Error: {e}. Using an Alternate Scraping XPATH....")
-                    item_data['event_date'] = self.getter.find_element(By.XPATH,"//time[contains(@data-automation,'event-details-time')]").text
-                    item_data['event_time'] = self.getter.find_element(By.XPATH,"//time[contains(@data-automation,'event-details-time')]").text
+                    try:
+                        item_data['event_date'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'event-right-col')]").text
+                        item_data['event_time'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'event-right-col')]").text
+                    except NoSuchElementException as e:
+                        logger.debug(f"Error: {e}. Using an Alternate Scraping XPATH....")
+                        item_data['event_date'] = self.getter.find_element(By.XPATH,"//time[contains(@data-automation,'event-details-time')]").text
+                        item_data['event_time'] = self.getter.find_element(By.XPATH,"//time[contains(@data-automation,'event-details-time')]").text
 
-                item_data['startups_contact_info'] = ''
-                item_data['event_link'] = link
+                    item_data['startups_contact_info'] = ''
+                    item_data['event_link'] = link
 
-                loaded_item = self.load_item(item_data=item_data,item_selector=i)
+                    yield self.load_item(item_data=item_data,item_selector=link)
 
-                logger.info(f"THIS IS THE LOADED ITEM --> {loaded_item}")
-
-                yield loaded_item
+        ####################
+        except Exception as e:
+            self.exception_handler(e)
--- spider_template.py
+++ spider_template.py
@@ -15,6 +15,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
+from functools import wraps
+
 
 class GGVenturesSpider(scrapy.Spider):
 
@@ -37,6 +39,10 @@ class GGVenturesSpider(scrapy.Spider):
 
     parse_code_link : str = ''
 
+    university_contact_info_xpath : str = ''
+    contact_info_text = False
+    contact_info_textContent = False
+
     item_data_empty = {
                         'university_name' : '',
                         'university_contact_info' : '',
@@ -55,6 +61,10 @@ class GGVenturesSpider(scrapy.Spider):
         self.eventbrite_api = EventBrite_API()
         self.start_time = round(time.time())
         self.scrape_time = None
+
+    def exception_handler(self,e):
+        logger.error(f"Experienced error on Spider: {self.name} --> {e}. Sending Error Email Notification")
+        error_email(self.name,e)
 
     def unique_event_checker(self,url_substring=''):
         if url_substring:
@@ -75,8 +85,6 @@ class GGVenturesSpider(scrapy.Spider):
 
     def load_item(self,item_data,item_selector):
 
-        logger.info("INSIDE THE LOAD ITEM CODE")
-
         data = ItemLoader(item = GgventuresItem(), selector = item_selector)
         data.add_value('university_name', self.static_name)
         data.add_value('university_contact_info',self.university_contact_info)
@@ -91,42 +99,45 @@ class GGVenturesSpider(scrapy.Spider):
         return data.load_item()
 
     def eventbrite_API_call(self,response):
-        if self.eventbrite_id:
-            # EVENTBRITE API - ORGANIZATION REQUEST
-            logger.info("Eventbrite ID Detected. Processing...")
-            raw_org = self.eventbrite_api.get_organizers(self.eventbrite_id)
+        try:
+            if self.eventbrite_id:
+                # EVENTBRITE API - ORGANIZATION REQUEST
+                logger.info("Eventbrite ID Detected. Processing...")
+                raw_org = self.eventbrite_api.get_organizers(self.eventbrite_id)
 
-            self.static_name = raw_org['name']
+                self.static_name = raw_org['name']
 
-            if raw_org['logo']:
-                logo = raw_org['logo']['url']
+                if raw_org['logo']:
+                    logo = raw_org['logo']['url']
+                else:
+                    logo = self.static_logo
+
+                # EVENTBRITE API - EVENT LIST REQUEST
+                raw_event = self.eventbrite_api.get_organizer_events(self.eventbrite_id)
+                last_page = int(raw_event['pagination']['page_count'])
+                prev_last_page = int(raw_event['pagination']['page_count']) - 1
+
+                event_list = self.eventbrite_api.get_organizer_events(self.eventbrite_id,page=prev_last_page)['events'] + self.eventbrite_api.get_organizer_events(self.eventbrite_id,page=last_page)['events']
+
+                for event in event_list:
+                    if datetime.strptime(event['start']['utc'].split('T')[0],'%Y-%m-%d') > datetime.utcnow():
+                        data = ItemLoader(item = GgventuresItem(), selector = event)
+                        data.add_value('university_name',self.static_name)
+                        data.add_value('university_contact_info',self.university_contact_info)
+                        data.add_value('logo',self.static_logo)
+                        data.add_value('event_name', event['name']['text'])
+                        data.add_value('event_desc', event['description']['text'])
+                        data.add_value('event_date', f"Start Date: {event['start']['utc']} - End Date: {event['end']['utc']}")
+                        data.add_value('event_link', event['url'])
+                        # data.add_value('event_time', event_time[i])
+                        yield data.load_item()
             else:
-                logo = self.static_logo
+                logger.debug(f"No EventBrite ID Number...")
 
-            # EVENTBRITE API - EVENT LIST REQUEST
-            raw_event = self.eventbrite_api.get_organizer_events(self.eventbrite_id)
-            last_page = int(raw_event['pagination']['page_count'])
-            prev_last_page = int(raw_event['pagination']['page_count']) - 1
-
-            event_list = self.eventbrite_api.get_organizer_events(self.eventbrite_id,page=prev_last_page)['events'] + self.eventbrite_api.get_organizer_events(self.eventbrite_id,page=last_page)['events']
-
-            for event in event_list:
-                if datetime.strptime(event['start']['utc'].split('T')[0],'%Y-%m-%d') > datetime.utcnow():
-                    data = ItemLoader(item = GgventuresItem(), selector = event)
-                    data.add_value('university_name',self.static_name)
-                    data.add_value('university_contact_info',self.university_contact_info)
-                    data.add_value('logo',self.static_logo)
-                    data.add_value('event_name', event['name']['text'])
-                    data.add_value('event_desc', event['description']['text'])
-                    data.add_value('event_date', f"Start Date: {event['start']['utc']} - End Date: {event['end']['utc']}")
-                    data.add_value('event_link', event['url'])
-                    # data.add_value('event_time', event_time[i])
-                    yield data.load_item()
-        else:
-            logger.debug(f"No EventBrite ID Number...")
-
-        logger.info("Proceed to Main Parse...")
-        yield scrapy.Request(url=self.parse_code_link,callback=self.parse_code)
+            logger.info("Proceed to Main Parse...")
+            yield scrapy.Request(url=self.parse_code_link,callback=self.parse_code)
+        except Exception as e:
+            self.exception_handler(e)
 
     def ClickMore(self,final_counter=10,start_counter=0):
         while True:
@@ -142,8 +153,14 @@ class GGVenturesSpider(scrapy.Spider):
                 logger.debug(f"No more Events to load --> {e}. Start Scraping......")
                 break
 
-    def get_university_contact_info(self):
-        pass
+    def get_university_contact_info(self,response):
+        self.driver.get(response.url)
+
+        if self.contact_info_text:
+            self.university_contact_info = (WebDriverWait(self.driver,60).until(EC.presence_of_element_located((By.XPATH, self.university_contact_info_xpath)))).text
+        elif self.contact_info_textContent:
+            self.university_contact_info = (WebDriverWait(self.driver,60).until(EC.presence_of_element_located((By.XPATH, self.university_contact_info_xpath)))).get_attribute('textContent')
+
 
     def parse_code(self,response):
         pass
@@ -151,12 +168,41 @@ class GGVenturesSpider(scrapy.Spider):
     def parse(self, response):
         try:
             self.get_university_contact_info(response)
-            logger.info("BEFORE EVENTBRITE CALL")
             yield scrapy.Request(url=response.url,callback=self.eventbrite_API_call)
-            # self.parse_code()
         except Exception as e:
-            logger.error(f"Experienced error on Spider: {self.name} --> {e}. Sending Error Email Notification")
-            error_email(self.name,e)
+            self.exception_handler(e)
+
+    def events_list(self,event_links_xpath:str):
+
+        web_elements_list = WebDriverWait(self.driver,40).until(EC.presence_of_all_elements_located((By.XPATH,event_links_xpath)))
+        return [x.get_attribute('href') for x in web_elements_list]
+
+
+    def multi_event_pages(self,num_of_pages=6,event_links_xpath='',next_page_xpath='',get_next_month=False,click_next_month=False):
+
+        event_links = []
+
+        for scrape_page in range(num_of_pages):
+            try:
+                web_elements_list = WebDriverWait(self.driver,40).until(EC.presence_of_all_elements_located((By.XPATH,event_links_xpath)))
+                event_links.extend([x.get_attribute('href') for x in web_elements_list])
+
+            except TimeoutException as e:
+                logger.debug(f"No available events for this month : {e} ---> Skipping...........")
+
+            try:
+                if get_next_month:
+                    next_month = self.driver.find_element(By.XPATH,"//a[contains(@title,'Go to next page')]").get_attribute('href')
+                    self.driver.get(next_month)
+
+                if click_next_month:
+                    WebDriverWait(self.driver,40).until(EC.element_to_be_clickable((By.XPATH,"//span[contains(@class,'next-btn')]"))).click()
+                # next_month = WebDriverWait(self.driver,20).until(EC.element_to_be_clickable((By.XPATH,"//a[contains(@title,'Go to the next page of the results')]"))).get_attribute('href')
+            except (TimeoutException,NoSuchElementException) as e:
+                logger.debug(f"Experienced Timeout Error on Spider: {self.name} --> {e}. Moving to the next spider...")
+                break
+
+        return event_links
 
     def closed(self, reason):
         try:
