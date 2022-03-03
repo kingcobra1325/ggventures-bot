08a59a4393c31918ea06177c68719d4aeb07b795
Namaedaa <joachim.cobar@gmail.com>
2022-03-03 23:43:28 +0800

    Updated Spiders

--- ggventures/spiders/can_0003.py
+++ ggventures/spiders/can_0003.py
@@ -1,10 +1,70 @@
-import scrapy
+from binaries import logger
+
+from spider_template import GGVenturesSpider
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 
-class Can0003Spider(scrapy.Spider):
+class Can0003Spider(GGVenturesSpider):
     name = 'can_0003'
-    allowed_domains = ['https://www.concordia.ca/jmsb.html']
-    start_urls = ['http://https://www.concordia.ca/jmsb.html/']
+    start_urls = ["https://www.concordia.ca/jmsb/programs/graduate/contact-us.html"]
+    country = 'Canada'
+    # eventbrite_id = 1412983127
 
-    def parse(self, response):
-        pass
+    # USE_HANDLE_HTTPSTATUS_LIST = False
+
+    static_name = "Concordia University,John Molson School of Business"
+    
+    static_logo = "https://icmrindia.org/just-in-case/Newsletter/image/logo6.jpg"
+
+    parse_code_link = "https://www.concordia.ca/jmsb/events.html"
+
+    university_contact_info_xpath = "//div[@class='rte']"
+    contact_info_text = True
+    # contact_info_textContent = False
+
+    def parse_code(self,response):
+        try:
+        ####################
+            self.driver.get(response.url)
+            
+            # self.ClickMore(click_xpath="//span[text()='Load More']",final_counter=3)
+
+            for link in self.events_list(event_links_xpath="//div[@class='rte']/a"):
+
+                self.getter.get(link)
+
+                if self.unique_event_checker(url_substring="https://www.concordia.ca/cuevents/jmsb/"):
+
+                    logger.info(f"Currently scraping --> {self.getter.current_url}")
+
+                    item_data = self.item_data_empty.copy()
+
+                    item_data['event_name'] = WebDriverWait(self.getter,20).until(EC.presence_of_element_located((By.XPATH,"//h1[@class='event-title']"))).get_attribute('textContent')
+                    item_data['event_link'] = link
+                    try:
+                        item_data['event_desc'] = self.getter.find_element(By.XPATH,"//div[@class='span8']").text
+                    except NoSuchElementException as e:
+                        logger.debug(f"Error: {e}. Using an Alternate Scraping XPATH....")
+                        item_data['event_desc'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'structured-content-rich-text')]").text
+
+                    try:
+                        item_data['event_date'] = self.getter.find_element(By.XPATH,"//div[@class='info-text']").text
+                        item_data['event_time'] = self.getter.find_element(By.XPATH,"//div[@class='info-text']").text
+                    except NoSuchElementException as e:
+                        logger.debug(f"Error: {e}. Using an Alternate Scraping XPATH....")
+                        # item_data['event_date'] = self.getter.find_element(By.XPATH,"").text
+                        # item_data['event_time'] = self.getter.find_element(By.XPATH,"").text
+
+                    item_data['startups_contact_info'] = self.getter.find_element(By.XPATH,"//h6[text()='CONTACT']/following-sibling::p").text
+                    # item_data['startups_link'] = ''
+                    # item_data['startups_name'] = ''
+
+                    yield self.load_item(item_data=item_data,item_selector=link)
+
+        ####################
+        except Exception as e:
+            self.exception_handler(e)
--- ggventures/spiders/can_0004.py
+++ ggventures/spiders/can_0004.py
@@ -1,10 +1,70 @@
-import scrapy
+from binaries import logger
+
+from spider_template import GGVenturesSpider
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 
-class Can0004Spider(scrapy.Spider):
+class Can0004Spider(GGVenturesSpider):
     name = 'can_0004'
-    allowed_domains = ['https://www.dal.ca/']
-    start_urls = ['http://https://www.dal.ca//']
+    start_urls = ["https://www.dal.ca/contact_us.html"]
+    country = 'Canada'
+    # eventbrite_id = 1412983127
 
-    def parse(self, response):
-        pass
+    # USE_HANDLE_HTTPSTATUS_LIST = False
+
+    static_name = "Dalhousie University"
+    
+    static_logo = "https://cdn.dal.ca/etc/designs/dalhousie/clientlibs/global/default/images/logos/dalhousie-logo-black.svg.lt_03b76acfc2f5fdf8c1ce5ef80071ef35.res/dalhousie-logo-black.svg"
+
+    parse_code_link = "https://events.dal.ca/"
+
+    university_contact_info_xpath = "//div[@class='maincontent']"
+    contact_info_text = True
+    # contact_info_textContent = False
+
+    def parse_code(self,response):
+        try:
+        ####################
+            self.driver.get(response.url)
+            
+            # self.ClickMore(click_xpath="//span[text()='Load More']",final_counter=3)
+
+            for link in self.events_list(event_links_xpath="//div[@class='dal-feature-title']/a"):
+
+                self.getter.get(link)
+
+                if self.unique_event_checker(url_substring="https://events.dal.ca/event/"):
+
+                    logger.info(f"Currently scraping --> {self.getter.current_url}")
+
+                    item_data = self.item_data_empty.copy()
+
+                    item_data['event_name'] = WebDriverWait(self.getter,20).until(EC.presence_of_element_located((By.XPATH,"//div[@id='lw_cal_events']/h1"))).get_attribute('textContent')
+                    item_data['event_link'] = link
+                    try:
+                        item_data['event_desc'] = self.getter.find_element(By.XPATH,"//div[@class='span8']").text
+                    except NoSuchElementException as e:
+                        logger.debug(f"Error: {e}. Using an Alternate Scraping XPATH....")
+                        item_data['event_desc'] = self.getter.find_element(By.XPATH,"//div[@class='lw_calendar_event_description']").text
+
+                    try:
+                        item_data['event_date'] = self.getter.find_element(By.XPATH,"//h5[@id='lw_cal_this_day']").text
+                        item_data['event_time'] = self.getter.find_element(By.XPATH,"//span[@class='lw_start_time']/..").text
+                    except NoSuchElementException as e:
+                        logger.debug(f"Error: {e}. Using an Alternate Scraping XPATH....")
+                        # item_data['event_date'] = self.getter.find_element(By.XPATH,"").text
+                        # item_data['event_time'] = self.getter.find_element(By.XPATH,"").text
+
+                    item_data['startups_contact_info'] = self.getter.find_element(By.XPATH,"//h4[text()='Contact']/following-sibling::p/a").text
+                    # item_data['startups_link'] = ''
+                    # item_data['startups_name'] = ''
+
+                    yield self.load_item(item_data=item_data,item_selector=link)
+
+        ####################
+        except Exception as e:
+            self.exception_handler(e)
