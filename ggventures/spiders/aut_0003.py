8c903dd4e80a9107f264a46b6e983ce592e0012e
kingcobra2alt <kingcobra1325@gmail.com>
2022-04-01 13:30:51 +0800

    Update aut_0003.py

--- ggventures/spiders/aut_0003.py
+++ ggventures/spiders/aut_0003.py
@@ -1,26 +1,21 @@
-from binaries import logger
 from spider_template import GGVenturesSpider
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-
 
 class Aut0003Spider(GGVenturesSpider):
     name = 'aut_0003'
-    start_urls = ['https://www.masterstudies.com/universities/Austria/Limak-ABS/']
+    start_urls = ['https://www.limak.at/kontakt/']
     country = "Austria"
     # eventbrite_id = 30819498834
+    TRANSLATE = True
 
     # handle_httpstatus_list = [301,302,403,404]
 
-    static_name = "https://www.jku.at/business-school/"
+    static_name = "LIMAK Johannes Kepler University Business School"
     static_logo = "https://keystoneacademic-res.cloudinary.com/image/upload/element/95/95140_thumb.jpg"
 
     # MAIN EVENTS LIST PAGE
-    parse_code_link = "https://www.donau-uni.ac.at/en/news/events.html"
+    parse_code_link = "https://www.limak.at/veranstaltungen"
 
-    university_contact_info_xpath = "//div[contains(@class,'richtext has-tooltip')]"
+    university_contact_info_xpath = "//h2/following-sibling::div"
     contact_info_text = True
     # contact_info_textContent = True
     # contact_info_multispan = True
@@ -29,36 +24,36 @@ class Aut0003Spider(GGVenturesSpider):
         try:
         ####################
             self.driver.get(response.url)
-            # self.check_website_changed(upcoming_events_xpath="//div[contains(@class,'site-content')]",empty_text=True)
-            # self.ClickMore(click_xpath="//a[contains(@class,'load-more')]",run_script=True)
-            # for link in self.multi_event_pages(num_of_pages=6,event_links_xpath="//li[contains(@class,'overview-normal')]/a",next_page_xpath="//a[contains(@class,'loadMoreButton')]",get_next_month=False,click_next_month=True,wait_after_loading=True):
-            for link in self.events_list(event_links_xpath="//div[contains(@class,'event-filter')]//div[contains(@class,'teaser-outer')]/a"):
+            # self.check_website_changed(upcoming_events_xpath="//p[contains(text(),'There are no upcoming events to display at this time.')]")
+            # self.ClickMore(click_xpath="//a[contains(@class,'btn--load-more')]",run_script=True)
+            for link in self.multi_event_pages(num_of_pages=8,event_links_xpath="//tbody//h3/a",next_page_xpath="//a[@rel='next']",get_next_month=True,click_next_month=False,wait_after_loading=False):
+                # for link in self.events_list(event_links_xpath="//div[contains(@class,'view-content row')]//a"):
                 self.getter.get(link)
-                if self.unique_event_checker(url_substring=['donau-uni.ac.at/en/university']):
+                if self.unique_event_checker(url_substring=['www.limak.at/veranstaltung']):
 
-                    logger.info(f"Currently scraping --> {self.getter.current_url}")
+                    self.Func.print_log(f"Currently scraping --> {self.getter.current_url}","info")
 
                     item_data = self.item_data_empty.copy()
 
-                    item_data['event_name'] = WebDriverWait(self.getter,20).until(EC.presence_of_element_located((By.XPATH,"//h1"))).text
-                    item_data['event_desc'] = self.getter.find_element(By.XPATH,"//main[contains(@id,'mainContent')]//div[contains(@class,'container')][3]").text
+                    item_data['event_name'] = self.Mth.WebDriverWait(self.getter,20).until(self.Mth.EC.presence_of_element_located((self.Mth.By.XPATH,"//h2[contains(@class,'vc_custom_heading')]"))).text
+                    item_data['event_desc'] = self.getter.find_element(self.Mth.By.XPATH,"//h2[contains(@class,'vc_custom_heading')]/following-sibling::div").text
+                    # item_data['event_desc'] = self.getter.find_element(self.Mth.By.XPATH,"//div[contains(@class,'event__body')]").text
 
-                    item_data['event_date'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'infoblock event')]").text
-                    item_data['event_time'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'infoblock event')]").text
-
+                    item_data['event_date'] = self.getter.find_element(self.Mth.By.XPATH,"//div[contains(@class,'tribe-events-schedule')]").get_attribute('textContent')
+                    item_data['event_time'] = self.getter.find_element(self.Mth.By.XPATH,"//div[contains(@class,'tribe-events-schedule')]").get_attribute('textContent')
                     # try:
                     #     item_data['event_date'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'modul-teaser__element')]").text
                     #     item_data['event_time'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'modul-teaser__element')]").text
-                    # except NoSuchElementException as e:
+                    # except self.Exc.NoSuchElementException as e:
                     #     logger.debug(f"XPATH not found {e}: Skipping.....")
                     #     # logger.debug(f"XPATH not found {e}: Skipping.....")
                     #     item_data['event_date'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'tile__content')]").text
                     #     item_data['event_time'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'tile__content')]").text
 
-                    try:
-                        item_data['startups_contact_info'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'peoplePicker')]/parent::div").text
-                    except NoSuchElementException as e:
-                        logger.debug(f"XPATH not found {e}: Skipping.....")
+                    # try:
+                    #     item_data['startups_contact_info'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'c-contact-card')]").text
+                    # except self.Exc.NoSuchElementException as e:
+                    #     logger.debug(f"XPATH not found {e}: Skipping.....")
                     # item_data['startups_link'] = ''
                     # item_data['startups_name'] = ''
                     item_data['event_link'] = link
