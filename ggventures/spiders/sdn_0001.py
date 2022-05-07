b52201efe888c8edb6f34b7966ea1dd7e5b89d0b
kingcobra2alt <kingcobra1325@gmail.com>
2022-05-07 17:47:29 +0800

    Added lka, sdn, sur, syr, sz spiders

--- ggventures/spiders/sdn_0001.py
+++ ggventures/spiders/sdn_0001.py
@@ -1,20 +1,20 @@
 from spider_template import GGVenturesSpider
 
 
-class Lka0001Spider(GGVenturesSpider):
-    name = 'lka_0001'
-    start_urls = ['https://mgmt.cmb.ac.lk/contact-us']
-    country = "Sri Lanka"
+class Sdn0001Spider(GGVenturesSpider):
+    name = 'sdn_0001'
+    start_urls = ['https://sms.uofk.edu/en/contact-us']
+    country = "Sudan"
     # eventbrite_id = 6552000185
     # TRANSLATE = True
 
     # handle_httpstatus_list = [301,302,403,404,429]
 
-    static_name = "University of Colombo,Faculty of Management and Finance"
-    static_logo = "https://mgmt.cmb.ac.lk/wp-content/uploads/2016/03/Uoc_FMF_New-1.jpg"
+    static_name = "University of Khartoum,School of Management Studies"
+    static_logo = "https://sms.uofk.edu/images/smslogo/sms_new.png"
 
     # MAIN EVENTS LIST PAGE
-    parse_code_link = "https://mgmt.cmb.ac.lk/event"
+    parse_code_link = "https://sms.uofk.edu/en/events-news"
 
     university_contact_info_xpath = "//body"
     # contact_info_text = True
@@ -28,10 +28,10 @@ class Lka0001Spider(GGVenturesSpider):
             # self.check_website_changed(upcoming_events_xpath="//div[@class='prodList']",empty_text=True)
             # self.ClickMore(click_xpath="//a[contains(@class,'moreListing')]",run_script=True)
             # self.Mth.WebDriverWait(self.driver, 10).until(self.Mth.EC.frame_to_be_available_and_switch_to_it((self.Mth.By.XPATH,"//iframe[@title='List Calendar View']")))
-            for link in self.multi_event_pages(num_of_pages=5,event_links_xpath="//h3/a",next_page_xpath="//a[@rel='next']",get_next_month=True):
-                # for link in self.events_list(event_links_xpath="//h3[contains(text(),'Upcoming Events')]/following-sibling::div//div[@class='programListItemSmall']/a[1]"):
+            # for link in self.multi_event_pages(num_of_pages=5,event_links_xpath="//h3/a",next_page_xpath="//a[@rel='next']",get_next_month=True):
+            for link in self.events_list(event_links_xpath="//div[contains(@class,'entry-image')]/a"):
                 self.getter.get(link)
-                if self.unique_event_checker(url_substring=['mgmt.cmb.ac.lk/event']):
+                if self.unique_event_checker(url_substring=['sms.uofk.edu/en/events-news']):
 
                     self.Func.print_log(f"Currently scraping --> {self.getter.current_url}","info")
 
@@ -39,18 +39,18 @@ class Lka0001Spider(GGVenturesSpider):
 
                     # self.Mth.WebDriverWait(self.driver, 10).until(self.Mth.EC.frame_to_be_available_and_switch_to_it((self.Mth.By.XPATH,"//iframe[@title='Event Detail']")))
 
-                    item_data['event_name'] = self.Mth.WebDriverWait(self.getter,20).until(self.Mth.EC.presence_of_element_located((self.Mth.By.XPATH,"//h1"))).text
-                    item_data['event_desc'] = self.getter.find_element(self.Mth.By.XPATH,"//div[@class='event-content']").text
+                    item_data['event_name'] = self.Mth.WebDriverWait(self.getter,20).until(self.Mth.EC.presence_of_element_located((self.Mth.By.XPATH,"//h2[@itemprop='name']"))).text
+                    item_data['event_desc'] = self.getter.find_element(self.Mth.By.XPATH,"//div[@itemprop='articleBody']").text
                     # try:
                     #     item_data['event_desc'] = self.getter.find_element(self.Mth.By.XPATH,"//div[@class'block-paragraph']/parent::div").text
                     # except self.Exc.NoSuchElementException as e:
                     #     self.Func.print_log(f"XPATH not found {e}: Skipping.....",'debug')
 
-                    item_data['event_date'] = self.getter.find_element(self.Mth.By.XPATH,"//div[@class='event-detail']").text
-                    item_data['event_time'] = self.getter.find_element(self.Mth.By.XPATH,"//div[@class='event-detail']").text
+                    # item_data['event_date'] = self.getter.find_element(self.Mth.By.XPATH,"//div[@class='event-detail']").text
+                    # item_data['event_time'] = self.getter.find_element(self.Mth.By.XPATH,"//div[@class='event-detail']").text
 
-                    # item_data['event_date'] = self.get_datetime_attributes("//time",'datetime')
-                    # item_data['event_time'] = self.get_datetime_attributes("//time",'datetime')
+                    item_data['event_date'] = self.get_datetime_attributes("//time",'datetime')
+                    item_data['event_time'] = self.get_datetime_attributes("//time",'datetime')
 
                     # try:
                     #     item_data['event_date'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'modul-teaser__element')]").text
---
+++ ggventures/spiders/sur_0001.py
@@ -0,0 +1,76 @@
+from spider_template import GGVenturesSpider
+
+
+class Sur0001Spider(GGVenturesSpider):
+    name = 'sur_0001'
+    start_urls = ['https://www.uvs.edu/contact/']
+    country = "Suriname"
+    # eventbrite_id = 6552000185
+    TRANSLATE = True
+
+    # handle_httpstatus_list = [301,302,403,404,429]
+
+    static_name = "Anton de Kom University"
+    static_logo = "https://www.uvs.edu/wp-content/uploads/2019/03/AdeKUSLogoHor_opt.png"
+
+    # MAIN EVENTS LIST PAGE
+    parse_code_link = "http://www.uvs.edu/events/"
+
+    university_contact_info_xpath = "//body"
+    # contact_info_text = True
+    contact_info_textContent = True
+    # contact_info_multispan = True
+
+    def parse_code(self,response):
+        try:
+        ####################
+            self.driver.get(response.url)
+            self.check_website_changed(upcoming_events_xpath="//td[contains(@class,'tribe-events-thismonth')]//a",checking_if_none=True)
+            # self.ClickMore(click_xpath="//a[contains(@class,'moreListing')]",run_script=True)
+            # self.Mth.WebDriverWait(self.driver, 10).until(self.Mth.EC.frame_to_be_available_and_switch_to_it((self.Mth.By.XPATH,"//iframe[@title='List Calendar View']")))
+            # for link in self.multi_event_pages(num_of_pages=5,event_links_xpath="//h3/a",next_page_xpath="//a[@rel='next']",get_next_month=True):
+            #     # for link in self.events_list(event_links_xpath="//h3[contains(text(),'Upcoming Events')]/following-sibling::div//div[@class='programListItemSmall']/a[1]"):
+            #     self.getter.get(link)
+            #     if self.unique_event_checker(url_substring=['mgmt.cmb.ac.lk/event']):
+            #
+            #         self.Func.print_log(f"Currently scraping --> {self.getter.current_url}","info")
+            #
+            #         item_data = self.item_data_empty.copy()
+            #
+            #         # self.Mth.WebDriverWait(self.driver, 10).until(self.Mth.EC.frame_to_be_available_and_switch_to_it((self.Mth.By.XPATH,"//iframe[@title='Event Detail']")))
+            #
+            #         item_data['event_name'] = self.Mth.WebDriverWait(self.getter,20).until(self.Mth.EC.presence_of_element_located((self.Mth.By.XPATH,"//h1"))).text
+            #         item_data['event_desc'] = self.getter.find_element(self.Mth.By.XPATH,"//div[@class='event-content']").text
+            #         # try:
+            #         #     item_data['event_desc'] = self.getter.find_element(self.Mth.By.XPATH,"//div[@class'block-paragraph']/parent::div").text
+            #         # except self.Exc.NoSuchElementException as e:
+            #         #     self.Func.print_log(f"XPATH not found {e}: Skipping.....",'debug')
+            #
+            #         item_data['event_date'] = self.getter.find_element(self.Mth.By.XPATH,"//div[@class='event-detail']").text
+            #         item_data['event_time'] = self.getter.find_element(self.Mth.By.XPATH,"//div[@class='event-detail']").text
+            #
+            #         # item_data['event_date'] = self.get_datetime_attributes("//time",'datetime')
+            #         # item_data['event_time'] = self.get_datetime_attributes("//time",'datetime')
+            #
+            #         # try:
+            #         #     item_data['event_date'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'modul-teaser__element')]").text
+            #         #     item_data['event_time'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'modul-teaser__element')]").text
+            #         # except self.Exc.NoSuchElementException as e:
+            #         #     self.Func.print_log(f"XPATH not found {e}: Skipping.....","debug")
+            #         #     # logger.debug(f"XPATH not found {e}: Skipping.....")
+            #         #     item_data['event_date'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'tile__content')]").text
+            #         #     item_data['event_time'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'tile__content')]").text
+            #
+            #         # try:
+            #         #     item_data['startups_contact_info'] = self.getter.find_element(self.Mth.By.XPATH,"//div[@class='staffList']").text
+            #         # except self.Exc.NoSuchElementException as e:
+            #         #     self.Func.print_log(f"XPATH not found {e}: Skipping.....",'debug')
+            #         # item_data['startups_link'] = ''
+            #         # item_data['startups_name'] = ''
+            #         item_data['event_link'] = link
+            #
+            #         yield self.load_item(item_data=item_data,item_selector=link)
+
+        ####################
+        except Exception as e:
+            self.exception_handler(e)
