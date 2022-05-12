e10e3c207f40f4760f62289fec1b01ef9c45a610
kingcobra2alt <kingcobra1325@gmail.com>
2022-05-12 11:14:27 +0800

    Added ago, dza spiders

---
+++ ggventures/spiders/dza_0002.py
@@ -0,0 +1,77 @@
+
+from spider_template import GGVenturesSpider
+
+
+class Dza0002Spider(GGVenturesSpider):
+    name = 'dza_0002'
+    start_urls = ['https://www.esc-alger.dz/']
+    country = "Algeria"
+    # eventbrite_id = 6552000185
+    TRANSLATE = True
+
+    # handle_httpstatus_list = [301,302,403,404,429]
+
+    static_name = "ESC Alger"
+    static_logo = "https://www.esaa.dz/wp-content/uploads/2016/08/logo-site-new-retina-blanc.png"
+
+    # MAIN EVENTS LIST PAGE
+    parse_code_link = "http://www.efsa.unsa.ba/ef/en/dogadaji"
+
+    university_contact_info_xpath = "//body"
+    contact_info_text = True
+    # contact_info_textContent = True
+    # contact_info_multispan = True
+
+    def parse_code(self,response):
+        try:
+        ####################
+            self.driver.get(response.url)
+            # self.check_website_changed(upcoming_events_xpath="//div[@id='content-bottom']//a",checking_if_none=True)
+            # self.ClickMore(click_xpath="//a[@id='btnLoadMore']",run_script=True)
+            # self.Mth.WebDriverWait(self.driver, 10).until(self.Mth.EC.frame_to_be_available_and_switch_to_it((self.Mth.By.XPATH,"//iframe[@title='List Calendar View']")))
+            # for link in self.multi_event_pages(num_of_pages=6,event_links_xpath="//h3/a",next_page_xpath="//a[@rel='next']",get_next_month=True):
+            for link in self.events_list(event_links_xpath="//*[contains(@class,'title')]/a"):
+                self.getter.get(link)
+                if self.unique_event_checker(url_substring=['www.umss.edu.bo']):
+
+                    self.Func.print_log(f"Currently scraping --> {self.getter.current_url}","info")
+
+                    item_data = self.item_data_empty.copy()
+
+                    # self.Mth.WebDriverWait(self.driver, 10).until(self.Mth.EC.frame_to_be_available_and_switch_to_it((self.Mth.By.XPATH,"//iframe[@title='Event Detail']")))
+
+                    item_data['event_name'] = self.Mth.WebDriverWait(self.getter,20).until(self.Mth.EC.presence_of_element_located((self.Mth.By.XPATH,"//div[@class='et_pb_text_inner']"))).text
+                    item_data['event_desc'] = self.getter.find_element(self.Mth.By.XPATH,"//div[contains(@class,'post_content_0_tb_body')]").text
+                    # try:
+                    #     item_data['event_desc'] = self.getter.find_element(self.Mth.By.XPATH,"//div[@class'block-paragraph']/parent::div").text
+                    # except self.Exc.NoSuchElementException as e:
+                    #     self.Func.print_log(f"XPATH not found {e}: Skipping.....",'debug')
+
+                    item_data['event_date'] = self.getter.find_element(self.Mth.By.XPATH,"//div[contains(@class,'et_pb_module et_pb_blurb')][2]").get_attribute('textContent')
+                    item_data['event_time'] = self.getter.find_element(self.Mth.By.XPATH,"//div[contains(@class,'post_content_0_tb_body')]").get_attribute('textContent')
+
+                    # item_data['event_date'] = self.get_datetime_attributes("//time",'datetime')
+                    # item_data['event_time'] = self.get_datetime_attributes("//time",'datetime')
+
+                    # try:
+                    #     item_data['event_date'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'modul-teaser__element')]").text
+                    #     item_data['event_time'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'modul-teaser__element')]").text
+                    # except self.Exc.NoSuchElementException as e:
+                    #     self.Func.print_log(f"XPATH not found {e}: Skipping.....","debug")
+                    #     # logger.debug(f"XPATH not found {e}: Skipping.....")
+                    #     item_data['event_date'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'tile__content')]").text
+                    #     item_data['event_time'] = self.getter.find_element(By.XPATH,"//div[contains(@class,'tile__content')]").text
+
+                    # try:
+                    #     item_data['startups_contact_info'] = self.getter.find_element(self.Mth.By.XPATH,"//div[@class='contactInfo']").get_attribute('textContent')
+                    # except self.Exc.NoSuchElementException as e:
+                    #     self.Func.print_log(f"XPATH not found {e}: Skipping.....",'debug')
+                    # item_data['startups_link'] = ''
+                    # item_data['startups_name'] = ''
+                    item_data['event_link'] = link
+
+                    yield self.load_item(item_data=item_data,item_selector=link)
+
+        ####################
+        except Exception as e:
+            self.exception_handler(e)
