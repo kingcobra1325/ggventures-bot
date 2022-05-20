ff1db5203404b2e6983013db9235f3ad1a3c8921
kingcobra2alt <kingcobra1325@gmail.com>
2022-05-20 15:31:25 +0800

    Updated bel_0007 and chl_0008 spiders

--- ggventures/spiders/chl_0008.py
+++ ggventures/spiders/chl_0008.py
@@ -2,20 +2,20 @@
 from spider_template import GGVenturesSpider
 
 
-class Bel0007Spider(GGVenturesSpider):
-    name = 'bel_0007'
-    start_urls = ['https://www.ugent.be/en/ghentuniv/contact']
-    country = "Belgium"
+class Chl0008Spider(GGVenturesSpider):
+    name = 'chl_0008'
+    start_urls = ['https://www.udec.cl/pexterno/contacto']
+    country = "Chile"
     # eventbrite_id = 6552000185
     TRANSLATE = True
 
     # handle_httpstatus_list = [301,302,403,404,429]
 
-    static_name = "Ghent University"
-    static_logo = r"https://upload.wikimedia.org/wikipedia/commons/b/be/Ghent_University_logo_%28English%29.png"
+    static_name = "Universidad de Concepción"
+    static_logo = "https://www.udec.cl/pexterno/sites/default/files/marca_udec2.png"
 
     # MAIN EVENTS LIST PAGE
-    parse_code_link = "https://www.ugent.be/en/agenda"
+    parse_code_link = "http://agenda.udec.cl/?q=calendar-node-field-fecha-evento-udec/month"
 
     university_contact_info_xpath = "//body"
     # contact_info_text = True
@@ -29,10 +29,10 @@ class Bel0007Spider(GGVenturesSpider):
             # self.check_website_changed(upcoming_events_xpath="//div[contains(@class,'all_posts_event')]//a",checking_if_none=True)
             # self.ClickMore(click_xpath="//a[@id='btnLoadMore']",run_script=True)
             # self.Mth.WebDriverWait(self.driver, 10).until(self.Mth.EC.frame_to_be_available_and_switch_to_it((self.Mth.By.XPATH,"//iframe[@title='List Calendar View']")))
-            # for link in self.multi_event_pages(num_of_pages=6,event_links_xpath="//h3/a",next_page_xpath="//a[contains(@class,'button-next')]",get_next_month=True):
-            for link in self.events_list(event_links_xpath="//li[@class='event']/a"):
+            for link in self.multi_event_pages(num_of_pages=6,event_links_xpath="//div[contains(@class,'monthview')]//a",next_page_xpath="//li[@class='next']/a",get_next_month=True):
+                # for link in self.events_list(event_links_xpath="//a[contains(@class,'title')]"):
                 self.getter.get(link)
-                if self.unique_event_checker(url_substring=['ugent.be/en/agenda']):
+                if self.unique_event_checker(url_substring=['agenda.udec.cl']):
 
                     self.Func.print_log(f"Currently scraping --> {self.getter.current_url}","info")
 
@@ -41,11 +41,11 @@ class Bel0007Spider(GGVenturesSpider):
                     # self.Mth.WebDriverWait(self.driver, 10).until(self.Mth.EC.frame_to_be_available_and_switch_to_it((self.Mth.By.XPATH,"//iframe[@title='Event Detail']")))
 
                     item_data['event_name'] = self.scrape_xpath(xpath_list=["//h1"])
-                    item_data['event_desc'] = self.scrape_xpath(xpath_list=["//div[@id='parent-fieldname-text']"],error_when_none=False)
-                    item_data['event_date'] = self.scrape_xpath(xpath_list=["//dl[@class='row']"],method='attr')
-                    item_data['event_time'] = self.scrape_xpath(xpath_list=["//dl[@class='row']"],method='attr')
+                    item_data['event_desc'] = self.scrape_xpath(xpath_list=["//div[contains(@class,'field-name-field-descripcion-evento')]"])
+                    item_data['event_date'] = self.scrape_xpath(xpath_list=["//span[@class='date-display-range']"],method='attr',error_when_none=False)
+                    item_data['event_time'] = self.scrape_xpath(xpath_list=["//div[@class='field-titulo-informacion-evento']"],method='attr')
 
-                    item_data['startups_contact_info'] = self.scrape_xpath(xpath_list=["//a[@class='url']"])
+                    item_data['startups_contact_info'] = self.scrape_xpath(xpath_list=["//div[@class='field-titulo-informacion-contacto']"],method='attr',error_when_none=True)
                     # item_data['startups_link'] = ''
                     # item_data['startups_name'] = ''
                     item_data['event_link'] = link
