c604cfced2b14900ef809e356b399dd4aa603ec1
kingcobra2alt <kingcobra1325@gmail.com>
2022-06-21 11:47:52 +0800

    updated spiders

--- ggventures/spiders/can_0006.py
+++ ggventures/spiders/can_0006.py
@@ -43,23 +43,15 @@ class Can0006Spider(GGVenturesSpider):
 
                     item_data = self.item_data_empty.copy()
 
-                    item_data['event_name'] = WebDriverWait(self.getter,20).until(EC.presence_of_element_located((By.XPATH,"//h1[@id='page-title']"))).get_attribute('textContent')
                     item_data['event_link'] = link
-                    try:
-                        item_data['event_desc'] = self.getter.find_element(By.XPATH,"//div[@class='channels-body-images']/following-sibling::div").text
-                    except NoSuchElementException as e:
-                        logger.debug(f"Error: {e}. Using an Alternate Scraping XPATH....")
-                        item_data['event_desc'] = self.getter.find_element(By.XPATH,"//div[@class='lw_calendar_event_description']").text
 
-                    try:
-                        item_data['event_date'] = self.getter.find_element(By.XPATH,"//div[@class='custom-multi-date']").text
-                        item_data['event_time'] = self.getter.find_element(By.XPATH,"//hr/following-sibling::h5").text
-                    except NoSuchElementException as e:
-                        logger.debug(f"Error: {e}. Using an Alternate Scraping XPATH....")
-                        # item_data['event_date'] = self.getter.find_element(By.XPATH,"").text
-                        # item_data['event_time'] = self.getter.find_element(By.XPATH,"").text
+                    item_data['event_name'] = self.scrape_xpath(xpath_list=["//h1[@id='page-title']"],method='attr')
+                    item_data['event_desc'] = self.scrape_xpath(xpath_list=["//div[@class='channels-body-images']/following-sibling::div","//div[@class='lw_calendar_event_description']"])
+                    item_data['event_date'] = self.scrape_xpath(xpath_list=["//div[@class='custom-multi-date']","//div[@class='custom-multi-date']"],method='attr')
+                    item_data['event_time'] = self.scrape_xpath(xpath_list=["//hr/following-sibling::h5","//div[@class='custom-multi-date']"],method='attr')
 
-                    item_data['startups_contact_info'] = self.getter.find_element(By.XPATH,"//h4[text()='Contact']/following-sibling::p/a").text
+
+                    item_data['startups_contact_info'] = self.scrape_xpath(xpath_list=["//h4[text()='Contact']/following-sibling::p/a"],method='attr',error_when_none=False,wait_time=5)
                     # item_data['startups_link'] = ''
                     # item_data['startups_name'] = ''
 
