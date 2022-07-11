6b0745e670ca40516ea171a7c3b1a1244c6058b3
kingcobra2alt <kingcobra1325@gmail.com>
2022-07-11 20:42:14 +0800

    Added pipelines memory leak optimizations

--- ggventures/pipelines.py
+++ ggventures/pipelines.py
@@ -1,6 +1,6 @@
 # Define your item pipelines here
 #
-import os,sys,traceback
+import os,sys,traceback,gc
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 from datetime import datetime, timezone
@@ -101,7 +101,8 @@ class CleanDataPipeline:
                     else:
                         logger.debug("PIPELINE: 'university_contact_info' empty...")
 
-
+                del adapter
+                gc.collect()
             else:
                 logger.info("PIPELINE: SKIPPED CLEANING DATA...")
 
@@ -167,6 +168,9 @@ class WriteDataPipeline:
 
             # ADD ITEM TO DF
             Add_Event(data=data,country_df=df,country_worksheet=worksheet,country=spider.country)
+
+            del adapter,data,df
+            gc.collect()
 
             return item
         except Exception as e:
@@ -257,8 +261,13 @@ class StartupsPipeline:
                     # ADD ITEM TO DF
                     Add_Startups_Event(data=data,startups_df=df,startups_worksheet=worksheet,country=spider.country)
 
+                    del data,df
+                    gc.collect()
+
                 else:
                     raise DropItem("PIPELINE: Event failed to meet Startup Keyword criteria...")
+                del adapter
+                gc.collect()
             else:
                 logger.info("PIPELINE: SKIPPING SORTING STARTUPS EVENTS...")
             return item
